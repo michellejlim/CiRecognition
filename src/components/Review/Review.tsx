9acfcfa14b29a31486608d11712f1244c17f2851
Vivian Huang <vivianh@andrew.cmu.edu>
2020-04-05 00:26:14 -0400

    add mock data for review

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -3,11 +3,56 @@ import "./Review.css";
 
 const apiUrl: string = "http://localhost:3000/api";
 
-type ReviewData = {
+type Review = {
   nominee: string;
   nominator: string;
   reason: string;
 };
+
+type Status = "approved" | "denied";
+type DoneReview = Review & { status: Status };
+
+type Response = {
+  pending: Review[];
+  past: DoneReview[];
+};
+
+function mockData(): Promise<Response> {
+  return Promise.resolve({
+    pending: [
+      {
+        nominator: "janice anne",
+        nominee: "sally handsen",
+        reason: "she's good",
+      },
+      {
+        nominator: "george washington",
+        nominee: "thomas jefferson",
+        reason: "good at writing",
+      },
+    ],
+    past: [
+      {
+        nominator: "vivian huang",
+        nominee: "jarrek holmes",
+        reason: "helpful guy",
+        status: "approved",
+      },
+      {
+        nominator: "andrew carnegie",
+        nominee: "andrew mellon",
+        reason: "good at art",
+        status: "approved",
+      },
+      {
+        nominator: "donald duck",
+        nominee: "thanos",
+        reason: "big and purple",
+        status: "denied",
+      },
+    ],
+  });
+}
 
 function Review() {
   return <p>hi</p>;
