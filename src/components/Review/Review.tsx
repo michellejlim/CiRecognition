f717eccdd5a97e8a5dd57e7beca70a33bdd71b2a
Vivian Huang <vivianh@andrew.cmu.edu>
2020-04-05 00:55:28 -0400

    added review page

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -3,29 +3,32 @@ import "./Review.css";
 
 const apiUrl: string = "http://localhost:3000/api";
 
-type Review = {
+type ReviewData = {
+  id: number;
   nominee: string;
   nominator: string;
   reason: string;
 };
 
 type Status = "approved" | "denied";
-type DoneReview = Review & { status: Status };
+type DoneReviewData = ReviewData & { status: Status };
 
 type Response = {
-  pending: Review[];
-  past: DoneReview[];
+  pending: ReviewData[];
+  past: DoneReviewData[];
 };
 
-function mockData(): Promise<Response> {
+function getResponse(): Promise<Response> {
   return Promise.resolve({
     pending: [
       {
+        id: 123123,
         nominator: "janice anne",
         nominee: "sally handsen",
         reason: "she's good",
       },
       {
+        id: 234234,
         nominator: "george washington",
         nominee: "thomas jefferson",
         reason: "good at writing",
@@ -33,18 +36,21 @@ function mockData(): Promise<Response> {
     ],
     past: [
       {
+        id: 345345,
         nominator: "vivian huang",
         nominee: "jarrek holmes",
         reason: "helpful guy",
         status: "approved",
       },
       {
+        id: 456456,
         nominator: "andrew carnegie",
         nominee: "andrew mellon",
         reason: "good at art",
         status: "approved",
       },
       {
+        id: 567567,
         nominator: "donald duck",
         nominee: "thanos",
         reason: "big and purple",
@@ -55,7 +61,37 @@ function mockData(): Promise<Response> {
 }
 
 function Review() {
-  return <p>hi</p>;
+  const [response, setResponse] = React.useState<Response | null>(null);
+  React.useEffect(() => {
+    // TODO what if the backend errors?
+    getResponse().then(setResponse).catch(console.error);
+  }, [setResponse]);
+  if (response === null) {
+    return null;
+  }
+  return (
+    <div className="noms">
+      <p>Nominations Pending Approval ({response.pending.length} Remaining)</p>
+      {response.pending.map((x) => (
+        <div key={x.id} className="pendingItem">
+          <button>APPROVE</button>
+          <button>DENY</button>
+          <p>Nominee: {x.nominee}</p>
+          <p>Nominator: {x.nominator}</p>
+          <p>Review: {x.reason}</p>
+        </div>
+      ))}
+      <p>Past Nominations ({response.past.length})</p>
+      {response.past.map((x) => (
+        <div key={x.id} className="pendingItem">
+          <p>Nominee: {x.nominee}</p>
+          <p>Nominator: {x.nominator}</p>
+          <p>Review: {x.reason}</p>
+          <p>{x.status}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Review;
