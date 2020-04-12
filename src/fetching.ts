cb9e67d9bd7644cb6ce1e8e6acdc2997aa7ea477
Vivian Huang <vivianh@andrew.cmu.edu>
2020-04-11 21:51:19 -0400

    type updates

--- src/fetching.ts
+++ src/fetching.ts
@@ -15,7 +15,24 @@ export function toJson(res: Response): Promise<any> {
 }
 
 export type Employee = {
+  id: number;
   employeeId: number;
   firstName: string;
   lastName: string;
+  emailCompany: string;
+  departmentName: string;
+  supervisorName: string;
+  supervisorEmployeeId: number;
+};
+
+export type NominationStatus = "pending" | "approved" | "denied";
+
+export type Nomination = {
+  id: number;
+  reason: string;
+  status: NominationStatus;
+  date: string;
+  nominator: number;
+  nominee: number;
+  award: number;
 };
